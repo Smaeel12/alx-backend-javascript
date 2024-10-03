4995a2f0b0308f6d7736737c16488183c3e147bf
Smaeel12 <ismailboubkri2002@gmail.com>
2024-10-03 03:07:20 +0100

    Correction of "2. Catch me if you can!"

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,8 +2,5 @@ export default function handleResponseFromAPI(promise) {
   promise.then(() => ({
     status: 200,
     body: 'success',
-  }), (err) => {
-    console.error('Error', err);
-    return new Error();
-  }).finally(() => console.log('Got a response from the API'));
+  }), () => new Error()).finally(() => console.log('Got a response from the API'));
 }
