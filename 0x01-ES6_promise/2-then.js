9983b6b0e3fd78c4c2c56a857787b0cc53947dc1
Smaeel12 <ismailboubkri2002@gmail.com>
2024-10-03 02:40:45 +0100

    the weirdo eslint

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,9 @@
 export default function handleResponseFromAPI(promise) {
   promise.then((value) => {
-    console.log("Got a response from the API")
-    return value
+    console.log('Got a response from the API');
+    return value;
   }, (err) => {
-    console.error("Error", err)
-    return new Error()
-  })
+    console.error('Error', err);
+    return new Error();
+  });
 }
--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,20 +1,17 @@
-import signUpUser from "./4-user-promise"
-import uploadPhoto from "./5-photo-reject"
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]).then((result) => {
-    [
-      {
-        status: result.status,
-        value: result.value
-      }
-    ]
-  }).catch((result) => {
-    [
-      {
-        status: result.status,
-        value: result.reason
-      }
-    ]
-  })
+  return Promise.allSettled([signUpUser(firstName, lastName),
+  uploadPhoto(fileName)]).then((result) => [
+    {
+      status: result.status,
+      value: result.value,
+    },
+  ]).catch((result) => [
+    {
+      status: result.status,
+      value: result.reason,
+    },
+  ]);
 }
