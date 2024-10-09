d06f35a6228c95519ea3d55a13bce2257554cae4
Smaeel12 <ismailboubkri2002@gmail.com>
2024-10-09 02:09:00 +0100

    Correction of "8. Clean set"

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,21 @@
 #!/usr/bin/node
-export default function cleanSet(set, startString) {
-  if (startString === '') return '';
-  const result = [];
-  for (const value of set) {
-    if (value.startsWith(startString)) {
-      result.push(value.slice(startString.length));
+export default (set, startString) => {
+  if (
+    !set && !startString && !(set instanceof Set) && typeof startString !== 'string'
+  ) {
+    return '';
+  }
+
+  const parts = [];
+
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
+
+      if (valueSubStr && valueSubStr !== value) {
+        parts.push(valueSubStr);
+      }
     }
   }
-  return result.join('-');
-}
+  return parts.join('-');
+};
