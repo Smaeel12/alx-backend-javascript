2a80d9c82a756128b25d61a9561095b9a043ea27
Smaeel12 <ismailboubkri2002@gmail.com>
2024-10-09 11:18:57 +0100

    fix 8-100

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,7 +2,7 @@
 const weakMap = new WeakMap();
 function queryAPI(endpoint) {
   const count = weakMap.get(endpoint) || 0;
-  if (weakMap.get(endpoint) >= 5) throw Error('Endpoint load is high');
   weakMap.set(endpoint, count + 1);
+  if (weakMap.get(endpoint) >= 5) throw Error('Endpoint load is high');
 }
 export { weakMap, queryAPI };
--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 export default (set, startString) => {
-  if (!startString || !Array.from(set).some((v) => v.startsWith(startString))) return '';
+  if (!startString
+    || !Array.from(set).some((v) => v.startsWith(startString))
+    || (!(startString instanceof String))) {
+    return '';
+  }
   const strs = Array.from(set)
     .map((v) => (v ? v.slice(startString.length) : undefined))
     .filter((x) => x);
